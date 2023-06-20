3372b94bdc4866177fef6cd7acc4004ba74e2533
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 15:21:04 +0300

    Ders5 tamamlandi.

--- Ders5/src/Main.java
+++ Ders5/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int sayi1 = 30;
+        int sayi2 = 25;
+        int sayi3 = 32;
+
+        int temp = sayi1;
+
+        if (sayi2 > temp) {
+            temp = sayi2;
+        } if (sayi3 > temp) {
+            temp = sayi3;
+        }
+
+        System.out.println("En buyuk sayi : " + temp);
     }
 }
