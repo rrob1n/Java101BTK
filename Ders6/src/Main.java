bbedec269498284ff31d2d56c07ae44f29e09076
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 15:34:22 +0300

    Ders6 tamamlandi.

--- Ders6/src/Main.java
+++ Ders6/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        char grade = 'A';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Mukemmel! : Gectiniz");
+                break;
+            case 'B':
+                System.out.println("Cok Guzel! : Gectiniz");
+                break;
+            case 'C':
+                System.out.println("Iyi! : Gectiniz");
+                break;
+            case 'D':
+                System.out.println("Fena Degil! : Gectiniz");
+                break;
+            case 'F':
+                System.out.println("Maalesef kaldiniz!");
+                break;
+            default:
+                System.out.println("Hatali giris yaptiniz!");
+        }
     }
 }
