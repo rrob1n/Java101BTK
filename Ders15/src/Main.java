1903008257c1b5fb1cf4c3f5eb8a079240d9a203
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 22:18:37 +0300

    Ders15 tamamlandi.

--- Ders15/src/Main.java
+++ Ders15/src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        // Arkadas Sayilar : Kendileri disinda pozitif bolenleri toplami digerine esit olan sayilardir.
+        // 220-284
+
+        int sayi1 = 220;
+        int sayi2 = 284;
+
+        int bolenToplam1 = 0;
+        int bolenToplam2 = 0;
+
+        for (int i = 1; i < sayi1; i++) {
+            if (sayi1 % i == 0) {
+                bolenToplam1 += i;
+            }
+        }
+
+        for (int i = 1; i < sayi2; i++) {
+            if (sayi2 % i == 0) {
+                bolenToplam2 += i;
+            }
+        }
+
+        if (bolenToplam1 == sayi2 && bolenToplam2 == sayi1){
+            System.out.println("Bu sayilar arkadastir.");
+        }else {
+            System.out.println("Arkadas degildir!");
+        }
     }
 }
