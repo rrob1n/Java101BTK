c009592b6a8d6303aca11b8e3ad1f37dc8687d43
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 21:50:38 +0300

    Alternatif cozum eklendi.

--- Ders12/src/Main.java
+++ Ders12/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Sayi asal sayi mi?
 
-        int number = 13;
+       /* int number = 13;
         int bolenSayisi = 0;
 
         for (int i = 1; i <= number ; i++) {
@@ -14,8 +14,23 @@ public class Main {
             System.out.println("Sayi asaldir!");
         }else {
             System.out.println("Sayi asal degildir!");
+        }*/
+
+        int number = 25;
+        int remainder = number %2;
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
         }
 
+        if (isPrime) {
+            System.out.println("Sayi asaldir!");
+        }else {
+            System.out.println("Sayi asal degildir!");
+        }
 
     }
 }
