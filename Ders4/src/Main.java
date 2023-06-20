21a4a35f7f6844090758ad113805c7f80fd10123
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 06:43:54 +0300

    Ders4 tamamlandi notlar eklendi.

--- Ders4/src/Main.java
+++ Ders4/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // icerideki condition saglanirsa true doner ve if blogu calisir.
+        // eger saglanmazsa else blogu calisir
+        // esittir icin "==" operatoru kullanilir.
+        // else if eger if disinda baska bir kosul varsa kullanilir.
+        // defensive programming : tum sartlara karsi program gelistirmek
+
+        int sayi = 14;
+
+        if (sayi < 20) {
+            System.out.println("Sayi 20 den kucuktur");
+        }else if (sayi == 20){
+            System.out.println("Sayi 20 dir");
+        }else {
+            System.out.println("Sayi 20 den buyuktur");
+        }
+
     }
 }
