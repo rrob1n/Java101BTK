d09ba2a7dc57dbfb6f9aa6fb65850117590a211b
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 22:09:41 +0300

    Ders14 tamamlandi.

--- Ders14/src/Main.java
+++ Ders14/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Mukemmel sayi : bolenlerinin toplami kendisi olan sayiya denir. (kendisi disinda)
+
+        int sayi = 28;
+        int bolenleriToplami = 0;
+
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                bolenleriToplami += i;
+            }
+        }
+        if (bolenleriToplami == sayi) {
+            System.out.println("Girdiginiz sayi mukemmel sayidir.");
+        } else {
+            System.out.println("Girdiginiz sayi bir mukemmel sayi degildir.");
+        }
     }
 }
