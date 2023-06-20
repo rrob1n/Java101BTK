aa601b94265929d98d386fd4930806f4d0f15145
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 06:19:45 +0300

    Ders3 tamamlandi notlar eklendi.

--- Ders3/src/Main.java
+++ Ders3/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // degisken atarken veri tipi belirtilir.
+        // primitive type : en temel veri tipleri.
+        // default olarak sayisal ifadeler int olarak kabul edilir.
+        // default olarak ondalikli sayilar double olarak kabul edilir.
+        // byte , short ,int ,long tam sayi
+        // float ve double ondalikl sayi tutar
+        // char tek bir karakteri tutar.
+        // boolean : dogru veya yanlis
+        // ASCII kodu her bir karakterin sayisal karsiligi
+
+        int sayi = 12;
+        char karakter = 'A';
+        boolean dogruMu = true;
     }
 }
