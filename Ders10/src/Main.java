7a46d81d8b794b70b7f5f24e38c525ab246fdece
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 17:31:31 +0300

    Ders10 tamamlandi.

--- Ders10/src/Main.java
+++ Ders10/src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String[][] sehirler = new String[3][3];
+
+        sehirler[0][0] = "Istanbul";
+        sehirler[0][1] = "Bursa";
+        sehirler[0][2] = "Bilecik";
+
+        sehirler[1][0] = "Ankara";
+        sehirler[1][1] = "Konya";
+        sehirler[1][2] = "Kayseri";
+
+        sehirler[2][0] = "Diyarbakir";
+        sehirler[2][1] = "Sanliurfa";
+        sehirler[2][2] = "Gaziantep";
+
+        for (int i = 0; i < 3; i++) {
+            if (i == 0) {
+                System.out.println("-----Marmara Bolgesi-----");
+            } else if (i == 1) {
+                System.out.println("-----Ic Anadolu Bolgesi-----");
+            } else {
+                System.out.println("-----Guneydogu Anadolu Bolgesi-----");
+            }
+            for (int j = 0; j < 3; j++) {
+
+                System.out.println(sehirler[i][j]);
+            }
+        }
     }
 }
