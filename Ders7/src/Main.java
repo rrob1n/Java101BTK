07c508a6e555817bb8e932e6640381ee03a4a1b7
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 16:23:57 +0300

    Ders8 ve Ders9 tamamlandi ve bu dosyaya eklendi.

--- Ders7/src/Main.java
+++ Ders7/src/Main.java
@@ -6,8 +6,28 @@ public class Main {
             System.out.println("Sayi : " + i);
         }
         System.out.println("Dongu bitti!");
-        for (int i = 0; i <= 10; i+=2) {
+        System.out.println("================================");
+        for (int i = 0; i <= 10; i += 2) {
             System.out.println("Sayi : " + i);
         }
+        System.out.println("Dongu bitti!");
+        System.out.println("================================");
+        //While Dongusu
+        int i = 1;
+        while (i < 10) {
+            System.out.println("Sayi : " +i);
+            i++;
+        }
+        System.out.println("Dongu bitti!");
+        System.out.println("================================");
+
+        //Do-While Dongusu
+
+        int i2 = 1;
+
+        do {
+            System.out.println("Sayimiz : " + i2);
+            i2++;
+        }while (i2 < 5);
     }
 }
