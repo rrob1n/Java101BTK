09cd1d85b3851f6a5e28d7deb6e19102a1b7763a
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 22:49:10 +0300

    Ders17 tamamlandi.

--- Ders17/src/Main.java
+++ Ders17/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+
     public static void main(String[] args) {
-        
+        sayiBulmaca();
+    }
+
+    public static void sayiBulmaca() {
+        int[] sayilar = new int[]{1, 3, 5, 7, 9, 0};
+        int aranacak = 5;
+
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+
+            if (sayi == aranacak) {
+                varMi = true;
+            }
+        }
+
+        if (varMi) {
+            mesajVer(aranacak);
+        } else {
+            System.out.println("Aradiginiz sayi dizide yok");
+        }
+    }
+    public static void mesajVer(int aranacak){
+        System.out.println("Aradiginiz sayi dizide var! : " + aranacak);
     }
 }
