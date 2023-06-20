17704b0ef9009edad32d50ba68e6afa708a5e4b7
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 22:56:34 +0300

    Ders18 tamamlandi.

--- Ders18/src/Main.java
+++ Ders18/src/Main.java
@@ -1,4 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        String mesaj = "Bugun hava cok guzel.";
+
+        int total = topla(5,7);
+        System.out.println(total);
+    }
+
+    public static void ekle() {
+        System.out.println("Eklendi!");
+    }
+
+    public static void sil() {
+        System.out.println("Silindi!");
+    }
+
+    public static void guncelle() {
+        System.out.println("Guncellendi");
+    }
+
+    public static int topla(int a, int b) {
+        return a + b;
+    }
+    public static String sehirVer(){
+        return "Ankara";
     }
 }
