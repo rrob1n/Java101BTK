452876940ede3bfaea99946ccee8df83faac7d4a
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 16:58:08 +0300

    Ders9 tamamlandi.

--- Ders9/src/Main.java
+++ Ders9/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+
+        //double[] myList = new double[4];
+        double[] myList = {1.2, 6.3, 4.3, 5.6};
+        double toplam = 0;
+        double enBuyuk = myList[0];
+
+        for (double sayi:myList) {
+            System.out.println(sayi);
+            if (sayi>enBuyuk){
+                enBuyuk = sayi;
+            }
+            toplam += sayi;
+        }
+        System.out.println("Sayilarin toplami : " + toplam);
+        System.out.println("Aralarindan en buyugu : " + enBuyuk);
     }
 }
