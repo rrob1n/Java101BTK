bdfb1998819f4f47e61f259b26f290f3596b1479
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 15:50:16 +0300

    Ders7 tamamlandi.

--- Ders7/src/Main.java
+++ Ders7/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //For Dongusu
+        for (int i = 1; i < 10; i++) {
+            System.out.println("Sayi : " + i);
+        }
+        System.out.println("Dongu bitti!");
+        for (int i = 0; i <= 10; i+=2) {
+            System.out.println("Sayi : " + i);
+        }
     }
 }
