b0b869bab00573497115a391a6c68b63ff829b59
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 21:47:22 +0300

    Ders12 tamamlandi.

--- Ders12/src/Main.java
+++ Ders12/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Sayi asal sayi mi?
+
+        int number = 13;
+        int bolenSayisi = 0;
+
+        for (int i = 1; i <= number ; i++) {
+            if (number % i == 0) {
+                bolenSayisi++;
+            }
+        }
+        if (bolenSayisi == 2 && number != 1) {
+            System.out.println("Sayi asaldir!");
+        }else {
+            System.out.println("Sayi asal degildir!");
+        }
+
+
     }
 }
