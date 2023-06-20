1658a486ae49a2fe7031f69f086d82724be9f06f
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-20 06:00:46 +0300

    Ders2 tamamlandi notlar eklendi.

--- Ders2/src/Main.java
+++ Ders2/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //java case sensitive bir dildir.
+        //degisken tanimlarken camel casing kullanilir.
+        //reusability : tekrar kullanilabilirlik.
+
+        int ogrenciSayisi = 10; // tam sayi turunde ismi ogrenciSayisi olan bir degisken tanimlamak.
+        String mesaj ="Ogrenci sayisi : "; //metinsel ifade
+
+        System.out.println("Ogrenci sayim : 9");
+        System.out.println("Ogrenci sayim : " + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+
     }
 }
