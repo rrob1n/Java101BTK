36ac3744a1745a2f2b3df91d8c91554cd6fb55c7
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-21 15:58:15 +0300

    Ders19 tamamlandi notlar eklendi.

--- Ders19/src/Main.java
+++ Ders19/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // classlar reference type dir.
+        CustomerManager customerManager;
+        CustomerManager customerManager1 = new CustomerManager();
+        customerManager = customerManager1;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+        // value type
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+
+        // arrayler referans tiptir
+        int[] sayilar1 = new int[]{1, 2, 3};
+        int[] sayilar2 = new int[]{4, 5, 6};
+
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
+
     }
 }
+
