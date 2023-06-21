74d307bf120e335291a21ef1ea9c6e3c8e9d5fdb
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-21 17:31:49 +0300

    Ders22 tamamlandi.

--- Ders22/src/DortIslem.java
+++ Ders22/src/DortIslem.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class DortIslem {
+public class DortIslem {
+
+    public int Topla(int s1, int s2) {
+        return s1 + s2;
+    }
+
+    public int Topla(int s1, int s2, int s3) {
+        return s1 + s2 + s3;
+    }
 }
