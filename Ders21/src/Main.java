66066efef7d4a4e87e5e84dfc5c5f22871d37c06
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-21 17:01:20 +0300

    Ders21 tamamlandi.

--- Ders21/src/Main.java
+++ Ders21/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Asus Laptop");
+        product.setPrice(5000.0);
+        product.setStockAmount(3);
+        product.setRenk("mavi");
+        System.out.println(product.getName());
+
+        System.out.println(product.getKod());
+
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+
     }
 }
--- Ders21/src/Product.java
+++ Ders21/src/Product.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Product {
+public class Product {
+    // attributes veya field denir bunlara
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stockAmount;
+    private String renk;
+    private String kod;
+
+    // getter
+    public int getId() {
+        return id;
+    }
+
+    // setter
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    public String getKod() {
+        return name.substring(0,1) + id;
+    }
+
+
 }
