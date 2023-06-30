357bd71c572a7bd463d244294d10b93eb325bc2e
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-30 20:06:36 +0300

    Ders35 tamamlandi.

--- Ders35/src/Main.java
+++ Ders35/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+        Customer engin = new Customer(1,"Engin","Demirog");
+        customers.add(engin);
+        customers.add(new Customer(2,"Derin","Demirog"));
+        customers.add(new Customer(3,"Salih","Demirog"));
+
+        customers.remove(engin);
+        for (Customer customer:customers) {
+            System.out.println(customer.firstName);
+        }
     }
 }
