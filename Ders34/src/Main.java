28dd003d526f33890b8f79f7d081b45f5e74f3dc
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-30 19:57:13 +0300

    Ders34 tamamlandi.

--- Ders34/src/Main.java
+++ Ders34/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> sehirler=new ArrayList<String>();
+        sehirler.add("Diyarbakir");
+        sehirler.add("İzmir");
+        sehirler.add("İstanbul");
+
+
+        sehirler.remove(2); // veya direk "İstanbul yazabilirsin"
+        Collections.sort(sehirler);
+        for (String sehir:sehirler) {
+            System.out.println(sehir);
+        }
+
     }
 }
