69bbc3ee80c427831ff877ed25f30b557a47fe14
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-30 20:18:37 +0300

    Ders36 tamamlandi.

--- Ders36/src/Main.java
+++ Ders36/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        HashMap<String, String> sozluk = new HashMap<String, String>();
+        sozluk.put("Book", "Kitap");
+        sozluk.put("Table", "Masa");
+        sozluk.put("Computer", "Bilgisayar");
+
+        System.out.println(sozluk.size());
+
+        for (String item : sozluk.keySet()) {
+            System.out.println("Eleman : " + item + " Deger : " + sozluk.get(item));
+        }
+        sozluk.remove("Table");
+        sozluk.clear();
+
+        System.out.println(sozluk.get("Table"));
     }
 }
