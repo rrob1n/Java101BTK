60ad78dcaa3a750459a1b0bae694844bd96489d8
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-06-30 19:49:50 +0300

    Ders33 tamamlandi.

--- Ders33/src/Main.java
+++ Ders33/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList sayilar = new ArrayList();
+
+        sayilar.add(1);
+        sayilar.add(10);
+        sayilar.add("Ankara");
+        System.out.println(sayilar.size());
+        System.out.println(sayilar.get(0));
+        sayilar.set(0,100);
+
+        System.out.println(sayilar.get(0));
+        sayilar.remove(1);
+        System.out.println(sayilar);
+        //sayilar.clear();
+
+        for (Object sayi:sayilar) {
+            System.out.println(sayi);
+        }
     }
 }
