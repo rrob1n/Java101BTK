6a531a2241c92b64ecf69914e08a69c0e84c6d9e
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 17:11:59 +0300

    Ders37 tamamlandi.

--- Ders37/src/Main.java
+++ Ders37/src/Main.java
@@ -3,9 +3,14 @@ public class Main {
         try {
             int[] sayilar = new int[]{1, 2, 3};
             System.out.println(sayilar[5]);
-        } catch (Exception exception) {
+        } catch (StringIndexOutOfBoundsException exception) {
             System.out.println("Hata Olustu!");
-        }finally {
+        }catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println(exception);
+        }catch (Exception exception){
+            System.out.println("Loglandi : " + exception);
+        }
+        finally {
             System.out.println("Her turlu calisir");
         }
 
