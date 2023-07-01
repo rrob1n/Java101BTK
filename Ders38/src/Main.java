e9ac1cbbed00ea2a97f81182379ce356a1e3aec2
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 19:46:07 +0300

    Ders38 tamamlandi.

--- Ders38/src/Main.java
+++ Ders38/src/Main.java
@@ -6,8 +6,14 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = null;
+        int total = 0;
         try {
             reader = new BufferedReader(new FileReader("/home/kurisu/Documents/repos/Java101BTK/Ders38/src/sayilar.txt"));
+            String line = null;
+            while((line = reader.readLine()) != null){
+                total += Integer.valueOf(line);
+            }
+            System.out.println("Total : " + total);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } finally {
