56983e6949c9bc97643ecb56358f7044918c15b0
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 16:53:48 +0300

    Try-catch-finally mantigini anlamak tamamlandi.

--- Ders37/src/Main.java
+++ Ders37/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            int[] sayilar = new int[]{1, 2, 3};
+            System.out.println(sayilar[5]);
+        } catch (Exception exception) {
+            System.out.println("Hata Olustu!");
+        }finally {
+            System.out.println("Her turlu calisir");
+        }
+
     }
 }
