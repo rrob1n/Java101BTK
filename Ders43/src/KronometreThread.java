de532762732ac780a99f2aa273d6ff36cb69af12
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-02 01:56:34 +0300

    Ders43 tamamlandi.

--- Ders43/src/KronometreThread.java
+++ Ders43/src/KronometreThread.java
@@ -1,2 +1,32 @@
-public class KronometreThread {
+public class KronometreThread implements Runnable {
+    private Thread thread;
+    private String threadName;
+
+    public KronometreThread(String threadName) {
+        this.threadName = threadName;
+        System.out.println("Olusturuluyor : " + threadName);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Calistiriliyor : " + threadName);
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(threadName + " : " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException exception) {
+            System.out.println("Kesildi : " + threadName);
+        }
+        System.out.println("Thread bitti : " + threadName);
+
+    }
+
+    public void start() {
+        System.out.println("Thread nesnesi olusuyor!");
+        if (thread == null) {
+            thread = new Thread(this,threadName);
+            thread.start();
+        }
+    }
 }
