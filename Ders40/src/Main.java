84d6c2021adc859ff4ead20de5400f9eac8e46ea
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 20:59:11 +0300

    Dosyalarin bilgilerini okumak tamamlandi.

--- Ders40/src/Main.java
+++ Ders40/src/Main.java
@@ -2,13 +2,29 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+        getFileInfo();
+    }
+
+    public static void createFile() throws IOException {
         File file = new File("/home/kurisu/Documents/repos/Java101BTK/files/students.txt");
         if (file.createNewFile()) {
             System.out.println("Dosya olusturuldu!");
-        }else {
+        } else {
             System.out.println("Bu dosya zaten var!");
         }
     }
-    
+
+    public static void getFileInfo() {
+        File file = new File("/home/kurisu/Documents/repos/Java101BTK/files/students.txt");
+        if (file.exists()) {
+            System.out.println("Dosya adi : " + file.getName());
+            System.out.println("Dosya yolu : " + file.getAbsolutePath());
+            System.out.println("Dosya yazilabilir mi : " + file.canWrite());
+            System.out.println("Dosya okunabilir mi : " + file.canRead());
+            System.out.println("Dosyanin boyutu (byte): " + file.length());
+        }
+    }
+
 }
