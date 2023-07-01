ad879fe64b6efdff06d5ab013de431f712e36e09
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 21:05:03 +0300

    Dosyalarin okunmasi tamamlandi.

--- Ders40/src/Main.java
+++ Ders40/src/Main.java
@@ -1,10 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         getFileInfo();
+        readFile();
     }
 
     public static void createFile() throws IOException {
@@ -24,6 +26,20 @@ public class Main {
             System.out.println("Dosya yazilabilir mi : " + file.canWrite());
             System.out.println("Dosya okunabilir mi : " + file.canRead());
             System.out.println("Dosyanin boyutu (byte): " + file.length());
+        }
+    }
+
+    public static void readFile() {
+        File file = new File("/home/kurisu/Documents/repos/Java101BTK/files/students.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
