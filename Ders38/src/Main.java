db1599ca02a9f096266d98b138ac4a7e4525fa66
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 17:29:06 +0300

    Checked exceptionlar part 1 tamamlandi.

--- Ders38/src/Main.java
+++ Ders38/src/Main.java
@@ -1,5 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/home/kurisu/Documents/repos/Java101BTK/Ders38/src/sayilar.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            reader.close();
+        }
     }
 }
