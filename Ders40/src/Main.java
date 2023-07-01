f24a2ff48daa656edbe9fa35de1819dcf658828d
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 20:51:25 +0300

    Dosya olusturma tamamlandi.

--- Ders40/src/Main.java
+++ Ders40/src/Main.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File file = new File("/home/kurisu/Documents/repos/Java101BTK/files/students.txt");
+        if (file.createNewFile()) {
+            System.out.println("Dosya olusturuldu!");
+        }else {
+            System.out.println("Bu dosya zaten var!");
+        }
     }
+    
 }
