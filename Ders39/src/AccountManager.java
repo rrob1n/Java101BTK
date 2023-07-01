4e7df53a7b9e49d81cb94f8ff558616f30b71773
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-07-01 20:14:03 +0300

    Throw ifadesiyle calismak tamamlandi.

--- Ders39/src/AccountManager.java
+++ Ders39/src/AccountManager.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class AccountManager {
+public class AccountManager {
+    private double balance;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws Exception {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            throw new Exception("Bakiye yetersiz!");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void printBalance() {
+        System.out.println("Hesap : " + getBalance());
+    }
 }
--- Ders39/src/Main.java
+++ Ders39/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        AccountManager accountManager = new AccountManager();
+
+        accountManager.deposit(100);
+        accountManager.printBalance();
+        try {
+            accountManager.withdraw(900);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
         
+
     }
 }
